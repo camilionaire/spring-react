0918307280184d94dba963e35055df008cb8c0a5
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2023-09-09 17:13:05 -0500

    simple web page up from ui 'npm start'

--- wisdom-pet/src/ui/src/App.js
+++ wisdom-pet/src/ui/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<Layout/>}>
+                    <Route index element={<Home/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
